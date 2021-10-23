f6d5d77877f5ce1e9f0a385caf676bf8a06d5b2c
reema <reemamousa@MacBook-Pro-Reema.local>
2021-10-23 16:43:50 +0300

    Creat Trivia Multiple Choice

---
+++ triviaMultipleChoice"Reema.Mousa"/triviaMultipleChoice"Reema.Mousa"/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  triviaMultipleChoice"Reema.Mousa"
+//
+//  Created by Reema Mousa on 16/03/1443 AH.
+//
+
+import Foundation
---
+++ triviaMultipleChoice"Reema.Mousa"/triviaMultipleChoice"Reema.Mousa"/Model/QuestionVariabel.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionVariabel.swift
+//  triviaMultipleChoice"Reema.Mousa"
+//
+//  Created by Reema Mousa on 16/03/1443 AH.
+//
+
+import Foundation
+struct Question {
+  let question :String
+  let answer :[String]
+  let correctAnswer :String
+
+
+
+}
+
+
+
