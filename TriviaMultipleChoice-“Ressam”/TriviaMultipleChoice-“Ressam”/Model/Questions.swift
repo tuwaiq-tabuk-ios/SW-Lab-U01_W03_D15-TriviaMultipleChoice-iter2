011b58cdc74308b620b37459bf70a1e86e35925e
Ressam Al-Thebailah <ralthebailah@MacBook-Air-Ressam.local>
2021-10-23 23:49:50 +0300

    Trivia Multiple Choice

---
+++ TriviaMultipleChoice-“Ressam”/TriviaMultipleChoice-“Ressam”/Model/Questions.swift
@@ -0,0 +1,13 @@
+//
+//  Questions.swift
+//  TriviaMultipleChoice-“Ressam”
+//
+//  Created by Ressam Al-Thebailah on 17/03/1443 AH.
+//
+
+import Foundation
+struct Question{
+  var question:String
+  var answers:[String]
+  var correctAnswer: String
+}
