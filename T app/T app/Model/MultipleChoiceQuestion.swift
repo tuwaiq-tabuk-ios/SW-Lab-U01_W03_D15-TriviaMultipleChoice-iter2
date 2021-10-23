fd860c65e623fbf067d2358b201b12918817efb5
rasha <rasha@MacBook-Air-rasha.local>
2021-10-23 16:13:12 +0300

    t app project

---
+++ T app/T app/Model/MultipleChoiceQuestion.swift
@@ -0,0 +1,14 @@
+//
+//  MultipleChoiceQuestion.swift
+//  T app
+//
+//  Created by rasha  on 16/03/1443 AH.
+//
+
+import Foundation
+
+struct MultipleChoiceQuestion {
+    let question: String
+    let answers: [String]
+    let correctAnswer: String
+}
