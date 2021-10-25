dbb269d408bb420983ac529b30c517cc00830d0e
hajer117 <h117hh@hotmail.com>
2021-10-25 09:05:39 +0300

    update

---
+++ TrivaMultpleChoice2"Hajermohamed"/TrivaMultpleChoice2"Hajermohamed"/model/MultlpleChoceQuestion.swift
@@ -0,0 +1,15 @@
+//
+//  MultlpleChoceQuestion.swift
+//  TrivaMultpleChoice2"Hajermohamed"
+//
+//  Created by hajer . on 18/03/1443 AH.
+//
+
+import Foundation
+
+struct MultipleChoiceQuestion {
+    let question: String
+    let answers: [String]
+    let correctAnswer: String
+}
+
