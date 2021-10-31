8284309a87e1184d2b7d90334e029a6467b1aae8
hajer117 <h117hh@hotmail.com>
2021-10-31 19:51:18 +0300

    TriviaMultiplechoice

---
+++ TriviaMultipleChoice-“hajer_mohamed”/TriviaMultipleChoice-“hajer_mohamed”/model/MultlpleChoceQuestion.swift
@@ -0,0 +1,16 @@
+//
+//  MultlpleChoceQuestion.swift
+//  TriviaMultipleChoice-“hajer_mohamed”
+//
+//  Created by hajer . on 25/03/1443 AH.
+//
+
+
+
+import Foundation
+
+struct MultipleChoiceQuestion {
+    let question: String
+    let answers: [String]
+    let correctAnswer: String
+}
